8881f04c6b04c08ae185b83a4b3b7995b1c88967
Kelvin Obidozie <kelvinobidozie@programmer.net>
2023-10-02 09:37:00 +0100

    Added Skills and About component

--- portfolio/src/components/Skill/Skill.js
+++ portfolio/src/components/Skill/Skill.js
@@ -1,55 +1,87 @@
-import React from 'react'
-import './Skill.css'
+import React from 'react';
+import './Skill.css';
+import logo from './assets/icons/html-5-logo-svgrepo-com.svg';
+import logo1 from './assets/icons/css-svgrepo-com.svg';
+import logo2 from './assets/icons/javascript-svgrepo-com.svg';
+
+import logo3 from './assets/icons/dart-svgrepo-com.svg';
+import logo4 from './assets/icons/flutter-svgrepo-com.svg';
+import logo10 from './assets/icons/nodejs-svgrepo-com.svg';
+import logo5 from './assets/icons/git-svgrepo-com.svg';
+import logo6 from './assets/icons/postman-icon-svgrepo-com.svg';
+import logo9 from './assets/icons/mongodb-svgrepo-com.svg';
+import logo7 from './assets/icons/vscode3-svgrepo-com.svg';
 
 function Skill() {
   return (
-    <div> <section class="skills" id="skills">
-      <h2 class="skill-header">My Top Skills</h2>
+    <div>
+      <section class="skills" id="skills">
+        <h2 class="skill-header">Skills and Technologies</h2>
 
-      <div class="skills-wrapper">
-        <div class="first-set animate__animated animate__pulse">
-          <img
-            src="assets/icons/icons8-html-5.svg"
-            alt=""
-            loading="lazy"
-            class="icon icon-card"
-          />
-          <img
-            src="assets/icons/icons8-css3.svg"
-            alt=""
-            loading="lazy"
-            class="icon icon-card"
-          />
-          <img
-            src="assets/icons/icons8-javascript.svg"
-            alt=""
-            loading="lazy"
-            class="icon icon-card"
-          />
-        </div>
+        <div class="skills-wrapper">
+          <div class="first-set animate__animated animate__pulse">
+            <img
+              src={logo}
+              alt="html"
+              loading="lazy"
+              class="icon icon-card"
+            />
+            <img
+              src={logo1}
+              alt=""
+              loading="lazy"
+              class="icon icon-card"
+            />
+            <img
+              src={logo2}
+              alt=""
+              loading="lazy"
+              class="icon icon-card"
+            />
+          </div>
 
-        <div class="second-set animate__animated animate__pulse">
-          <img
-            src="assets/icons/icons8-bootstrap.svg"
-            alt=""
-            loading="lazy"
-            class="icon icon-card"
-          />
-          <img
-            src="assets/icons/icons8-react-native.svg"
-            alt=""
-            loading="lazy"
-            class="icon icon-card"
-          />
-          <img
-            src="assets/icons/icons8-git.svg"
-            alt=""
-            loading="lazy"
-            class="icon icon-card"
-          />
+          <div class="second-set animate__animated animate__pulse">
+            <img
+              src={logo3}
+              alt=""
+              loading="lazy"
+              class="icon icon-card"
+            />
+            <img
+              src={logo4}
+              alt=""
+              loading="lazy"
+              class="icon icon-card"
+            />
+            <img
+              src={logo5}
+              alt=""
+              loading="lazy"
+              class="icon icon-card"
+            />
+          </div>
+          <div class="second-set animate__animated animate__pulse">
+            <img
+              src={logo6}
+              alt=""
+              loading="lazy"
+              class="icon icon-card"
+            />
+            <img
+              src={logo7}
+              alt=""
+              loading="lazy"
+              class="icon icon-card"
+            />
+            <img
+              src={logo9}
+              alt=""
+              loading="lazy"
+              class="icon icon-card"
+            />
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
     </div>
   )
 }
--- portfolio/src/components/navbar/Navbar.js
+++ portfolio/src/components/navbar/Navbar.js
@@ -8,26 +8,26 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
       <div className="container">
-        <a className="navbar-brand" href="#"><img className='logo' src={logo} alt="logo" /></a>
+        <a className="navbar-brand" href="#home"><img className='logo' src={logo} alt="logo" /></a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <FontAwesomeIcon icon={faBars} style={{ color: '#fff' }} />
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#">Home</a>
+              <a className="nav-link" aria-current="page" href="#home">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">About me</a>
+              <a className="nav-link" href="#about">About me</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Services</a>
+              <a className="nav-link" href="#services">Services</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Portfolio</a>
+              <a className="nav-link" href="#portfolio">Portfolio</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Contact me</a>
+              <a className="nav-link" href="#contact">Contact me</a>
             </li>
           </ul>
         </div>
